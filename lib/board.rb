b5b821588ea1fd482d6e67ec7b8f23d32cc6b90f
Adam W <adamw207@yahoo.com>
2019-04-05 23:56:03 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -9,24 +9,6 @@ board = Array
 row = [" ", " ", " ", " ", " ", " ", " ", " "]
 
  
-# Position 1: Left
-row[0]
- 
-# Position 2: Middle
-row[1]
- 
-# Position 3: Right
-row[2]
- 
-# Position 4: Left
-row[0]
-
-# Position 5: Middle
-row[1]
-
-# Position 6:Right
-row[2]
-
 
 
 def display_board(board)
