0505206638d6a0f5b5bf6cb0f61e60be96e48832
Adam W <adamw207@yahoo.com>
2019-04-05 23:55:11 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -44,3 +44,12 @@ row[1] = "x"
 row #=> [" ", "x", " "]
 row[2] = "x"
 row #=> [" ", "x", " "]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+ 
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
